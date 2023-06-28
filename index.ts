041ce7035574186cef30d18c8b17ef747539d630
Julien <julien@ripouteau.com>
2023-06-28 20:55:33 +0200

    refactor: update to latest @japa/runner

--- index.ts
+++ index.ts
@@ -7,16 +7,17 @@
  * file that was distributed with this source code.
  */
 
-import type { PluginFn } from '@japa/runner'
 import { expectTypeOf as expectType } from 'expect-type'
+import type { PluginFn } from '@japa/runner/types'
+import { TestContext } from '@japa/runner/core'
 
 export function expectTypeOf(): PluginFn {
-  return function (_, __, { TestContext }) {
+  return function () {
     TestContext.macro('expectTypeOf', expectType)
   }
 }
 
-declare module '@japa/runner' {
+declare module '@japa/runner/core' {
   interface TestContext {
     expectTypeOf: typeof expectType
   }
